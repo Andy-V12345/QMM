c302702c12349f4be7a0f7ab189b0ae5be26d8f6
Andy Vu <andyvu@macbook-pro.wireless.northwestern.private>
2023-05-08 16:30:37 -0500

    created project

---
+++ QuickMentalMath/QuickMentalMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickMentalMathApp.swift
+//  QuickMentalMath
+//
+//  Created by Andy Vu on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct QuickMentalMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
