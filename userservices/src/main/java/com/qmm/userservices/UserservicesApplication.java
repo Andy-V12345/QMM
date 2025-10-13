4143d4118861178cd8b214016b163a9c124966e1
Andy Vu <andyvu2026@u.northwestern.edu>
2025-10-13 11:09:28 -0500

    Cleaned up backend and set up env reading

--- userservices/src/main/java/com/qmm/userservices/UserservicesApplication.java
+++ userservices/src/main/java/com/qmm/userservices/UserservicesApplication.java
@@ -3,10 +3,20 @@ package com.qmm.userservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class UserservicesApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
+		System.out.println(System.getProperty("GOOGLE_APPLICATION_CREDENTIALS"));
+		
 		SpringApplication.run(UserservicesApplication.class, args);
 	}
 
