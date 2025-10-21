f4523b726458d3627356992a82be2c2a8b0e4e0d
Andy Vu <andyvu2026@u.northwestern.edu>
2025-10-21 15:05:23 -0500

    updated gitignore for google creds

---
+++ QuickMentalMath/Views/MultiplayerGameView.swift
@@ -0,0 +1,18 @@
+//
+//  MultiplayerGameView.swift
+//  QuickMentalMath
+//
+//  Created by Andy Vu on 10/20/25.
+//
+
+import SwiftUI
+
+struct MultiplayerGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultiplayerGameView()
+}
